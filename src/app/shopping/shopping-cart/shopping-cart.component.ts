e625b08a9f089d8f84ad0c49b7aa95f217e6cc98
François Boiteux <francois.boiteux@epita.fr>
2019-09-24 21:40:38 +1000

    feat(shopping): Implement logic of shopping-cart and shopping-items

--- src/app/shopping/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { get, orderBy, sortBy } from 'lodash';
+
+import { ICartProduct } from '../shopping.model';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.scss']
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnChanges {
+  @Input() cartProducts: ICartProduct[];
+  @Input() cartTotal: number;
+  @Output() productDelete: EventEmitter<{ productId: number }> = new EventEmitter();
+  @Output() productQuantityIncrease: EventEmitter<{ productId: number }> = new EventEmitter();
+  @Output() productQuantityDecrease: EventEmitter<{ productId: number }> = new EventEmitter();
 
-  constructor() { }
+  filteredProducts: ICartProduct[];
+  sortedColumn = 'name';
+  sortOrder = 'desc';
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.cartProducts) {
+      this.applyFilterProducts();
+    }
   }
 
+  updateColumnSorting(column: string) {
+    if (this.sortedColumn !== column) {
+      this.sortOrder = 'desc';
+    } else {
+      switch (this.sortOrder) {
+        case '':
+          this.sortOrder = 'desc';
+          break;
+        case 'desc':
+          this.sortOrder = 'asc';
+          break;
+        default:
+          this.sortOrder = '';
+          break;
+      }
+    }
+
+    this.sortedColumn = column;
+    this.applyFilterProducts();
+  }
+
+  private applyFilterProducts(): void {
+    this.filteredProducts =
+      this.sortOrder !== ''
+        ? orderBy(
+            this.cartProducts,
+            [`product.${this.sortedColumn}`],
+            [this.sortOrder === 'desc' ? 'desc' : 'asc']
+          )
+        : this.cartProducts;
+  }
 }
--- src/app/shopping/shopping-items/shopping-items.component.ts
+++ src/app/shopping/shopping-items/shopping-items.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Dictionary, get } from 'lodash';
+
+import { IProduct } from '../shopping.model';
 
 @Component({
   selector: 'app-shopping-items',
   templateUrl: './shopping-items.component.html',
   styleUrls: ['./shopping-items.component.scss']
 })
-export class ShoppingItemsComponent implements OnInit {
+export class ShoppingItemsComponent {
+  @Input() products: IProduct[];
+  @Input() productsMap: Dictionary<IProduct>;
+  @Output() productAdd: EventEmitter<{ product: IProduct; quantity: number }> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  get productSelected() {
+    return get(this.productsMap, this.selectionForm.get('productId').value);
   }
 
+  get quantitySelected() {
+    return this.selectionForm.get('quantity').value;
+  }
+
+  get quantityFieldHasErrors() {
+    return this.selectionForm.get('quantity').invalid;
+  }
+
+  selectionForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.initForm();
+  }
+
+  addProduct(product: IProduct, quantity: number): void {
+    this.productAdd.emit({ product, quantity });
+  }
+
+  private initForm(): void {
+    this.selectionForm = this.formBuilder.group({
+      productId: [0, [Validators.required]],
+      quantity: [1, [Validators.required, Validators.min(1)]]
+    });
+  }
 }
