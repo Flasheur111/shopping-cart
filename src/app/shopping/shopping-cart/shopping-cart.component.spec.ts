1e6d55b6d439e0447fac921ad48edde91779680b
François Boiteux <francois.boiteux@epita.fr>
2019-09-24 22:20:38 +1000

    test(shopping): Add unit tests for shopping-cart and shopping-items

--- src/app/shopping/shopping-cart/shopping-cart.component.spec.ts
+++ src/app/shopping/shopping-cart/shopping-cart.component.spec.ts
@@ -1,4 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { times } from 'lodash';
+import { TestUtils } from 'src/test/test-utils';
+import { Subscription } from 'rxjs';
+import { SimpleChange } from '@angular/core';
+
+import { ICartProduct, IProduct } from '../shopping.model';
 
 import { ShoppingCartComponent } from './shopping-cart.component';
 
@@ -6,20 +14,271 @@ describe('ShoppingCartComponent', () => {
   let component: ShoppingCartComponent;
   let fixture: ComponentFixture<ShoppingCartComponent>;
 
+  let cartProducts: ICartProduct[];
+  let cartTotal: number;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShoppingCartComponent ]
-    })
-    .compileComponents();
+      imports: [CommonModule, TranslateModule.forRoot()],
+      declarations: [ShoppingCartComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ShoppingCartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    cartProducts = times(3, index => ({
+      ...TestUtils.createSpyObjWithoutMethods<ICartProduct>(`cartProducts-${index}`),
+      quantity: index
+    }));
+    cartTotal = 10;
+    component.cartProducts = cartProducts;
+    component.cartTotal = cartTotal;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a cartProducts input', () => {
+    expect(component.cartProducts).toEqual(cartProducts);
+  });
+
+  it('should have a cartTotal input', () => {
+    expect(component.cartTotal).toEqual(cartTotal);
+  });
+
+  describe('when deleting a product', () => {
+    let event: any;
+    let subscription: Subscription;
+    let productId: number;
+
+    beforeEach(() => {
+      event = jasmine.createSpy('event');
+      subscription = component.productDelete.subscribe(event);
+
+      productId = 123;
+      component.productDelete.emit({ productId });
+    });
+
+    afterEach(() => {
+      subscription.unsubscribe();
+    });
+
+    it('should emit an event with productId', () => {
+      expect(event).toHaveBeenCalledWith({ productId });
+    });
+  });
+
+  describe('when increasing a product quantity', () => {
+    let event: any;
+    let subscription: Subscription;
+    let productId: number;
+
+    beforeEach(() => {
+      event = jasmine.createSpy('event');
+      subscription = component.productQuantityIncrease.subscribe(event);
+
+      productId = 123;
+      component.productQuantityIncrease.emit({ productId });
+    });
+
+    afterEach(() => {
+      subscription.unsubscribe();
+    });
+
+    it('should emit an event with productId', () => {
+      expect(event).toHaveBeenCalledWith({ productId });
+    });
+  });
+
+  describe('when decreasing a product quantity', () => {
+    let event: any;
+    let subscription: Subscription;
+    let productId: number;
+
+    beforeEach(() => {
+      event = jasmine.createSpy('event');
+      subscription = component.productQuantityDecrease.subscribe(event);
+
+      productId = 123;
+      component.productQuantityDecrease.emit({ productId });
+    });
+
+    afterEach(() => {
+      subscription.unsubscribe();
+    });
+
+    it('should emit an event with productId', () => {
+      expect(event).toHaveBeenCalledWith({ productId });
+    });
+  });
+
+  describe('when changes are detected', () => {
+    describe('when cartProducts is changing', () => {
+      beforeEach(() => {
+        cartProducts = [
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'a'
+            }
+          },
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'b'
+            }
+          }
+        ];
+        component.cartProducts = cartProducts;
+        component.ngOnChanges({
+          cartProducts: TestUtils.createSpyObjWithoutMethods<SimpleChange>('change')
+        });
+      });
+
+      it('should reorder cartProducts on name column by descending order', () => {
+        expect(component.filteredProducts).toEqual([cartProducts[1], cartProducts[0]]);
+      });
+    });
+  });
+
+  describe('when rotating column sorting', () => {
+    describe('when sorting is descending', () => {
+      beforeEach(() => {
+        component.sortOrder = 'desc';
+        cartProducts = [
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'a'
+            }
+          },
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'b'
+            }
+          }
+        ];
+        component.cartProducts = cartProducts;
+        component.updateColumnSorting('name');
+      });
+
+      it('should update the sortOrder to ascending', () => {
+        expect(component.sortOrder).toEqual('asc');
+      });
+
+      it('should reorder cartProducts on name column by ascending order', () => {
+        expect(component.filteredProducts).toEqual(cartProducts);
+      });
+    });
+
+    describe('when sorting is ascending', () => {
+      beforeEach(() => {
+        component.sortOrder = 'asc';
+        cartProducts = [
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'a'
+            }
+          },
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'b'
+            }
+          }
+        ];
+        component.cartProducts = cartProducts;
+        component.updateColumnSorting('name');
+      });
+
+      it('should reset the sortOrder', () => {
+        expect(component.sortOrder).toEqual('');
+      });
+
+      it('should NOT reorder cartProducts on name column', () => {
+        expect(component.filteredProducts).toEqual(cartProducts);
+      });
+    });
+
+    describe('when sorting is empty', () => {
+      beforeEach(() => {
+        component.sortOrder = '';
+        cartProducts = [
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'a'
+            }
+          },
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'b'
+            }
+          }
+        ];
+        component.cartProducts = cartProducts;
+        component.updateColumnSorting('name');
+      });
+
+      it('should update the sortOrder to descending', () => {
+        expect(component.sortOrder).toEqual('desc');
+      });
+
+      it('should reorder cartProducts on name column by descending order', () => {
+        expect(component.filteredProducts).toEqual([cartProducts[1], cartProducts[0]]);
+      });
+    });
+
+    describe('when a new column is selected', () => {
+      beforeEach(() => {
+        component.sortOrder = 'desc';
+        cartProducts = [
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'a',
+              price: 1
+            }
+          },
+          {
+            quantity: 1,
+            product: {
+              ...TestUtils.createSpyObjWithoutMethods<IProduct>('product'),
+              name: 'b',
+              price: 2
+            }
+          }
+        ];
+        component.cartProducts = cartProducts;
+        component.updateColumnSorting('price');
+      });
+
+      it('should set the sortOrder to descending', () => {
+        expect(component.sortOrder).toEqual('desc');
+      });
+
+      it('should set the sortedColumn to the new column', () => {
+        expect(component.sortedColumn).toEqual('price');
+      });
+
+      it('should reorder cartProducts on price column by descending order', () => {
+        expect(component.filteredProducts).toEqual([cartProducts[1], cartProducts[0]]);
+      });
+    });
   });
 });
--- src/app/shopping/shopping-items/shopping-items.component.spec.ts
+++ src/app/shopping/shopping-items/shopping-items.component.spec.ts
@@ -1,4 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { Dictionary, times } from 'lodash';
+import { Subscription } from 'rxjs';
+import { TestUtils } from 'src/test/test-utils';
+
+import { IProduct } from '../shopping.model';
 
 import { ShoppingItemsComponent } from './shopping-items.component';
 
@@ -6,20 +13,160 @@ describe('ShoppingItemsComponent', () => {
   let component: ShoppingItemsComponent;
   let fixture: ComponentFixture<ShoppingItemsComponent>;
 
+  let products: IProduct[];
+  let productsMap: Dictionary<IProduct>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShoppingItemsComponent ]
-    })
-    .compileComponents();
+      imports: [
+        // Angular
+        FormsModule,
+        ReactiveFormsModule,
+        TranslateModule.forRoot()
+      ],
+      declarations: [ShoppingItemsComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ShoppingItemsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    products = times(3, index => ({
+      ...TestUtils.createSpyObjWithoutMethods<IProduct>(`product-${index}`),
+      id: index
+    }));
+    productsMap = {
+      1: {
+        ...TestUtils.createSpyObjWithoutMethods<IProduct>('product-1'),
+        id: 1
+      }
+    };
+    component.products = products;
+    component.productsMap = productsMap;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a products input', () => {
+    expect(component.products).toEqual(products);
+  });
+
+  it('should have a productsMap input', () => {
+    expect(component.productsMap).toEqual(productsMap);
+  });
+
+  describe('when adding a product', () => {
+    let event: any;
+    let subscription: Subscription;
+    let payload: any;
+
+    beforeEach(() => {
+      event = jasmine.createSpy('event');
+      subscription = component.productAdd.subscribe(event);
+
+      payload = { product: TestUtils.createSpyObjWithoutMethods<IProduct>('product'), quantity: 1 };
+      component.productAdd.emit(payload);
+    });
+
+    afterEach(() => {
+      subscription.unsubscribe();
+    });
+
+    it('should emit an event with product and quantity added', () => {
+      expect(event).toHaveBeenCalledWith(payload);
+    });
+  });
+
+  describe('when constructing', () => {
+    it('should initialise and default the form', () => {
+      expect(component.selectionForm.value).toEqual({
+        productId: 0,
+        quantity: 1
+      });
+    });
+  });
+
+  describe('when validating the form', () => {
+    describe('the productId control', () => {
+      describe('if empty', () => {
+        beforeEach(() => {
+          component.selectionForm.get('productId').setValue('');
+        });
+
+        it('should be invalid', () => {
+          expect(component.selectionForm.get('productId').invalid).toEqual(true);
+        });
+      });
+
+      describe('if NOT empty', () => {
+        beforeEach(() => {
+          component.selectionForm.get('productId').setValue(2);
+        });
+
+        it('should be invalid', () => {
+          expect(component.selectionForm.get('productId').valid).toEqual(true);
+        });
+      });
+    });
+
+    describe('the quantity control', () => {
+      describe('if empty', () => {
+        beforeEach(() => {
+          component.selectionForm.get('quantity').setValue('');
+        });
+
+        it('should be invalid', () => {
+          expect(component.selectionForm.get('quantity').invalid).toEqual(true);
+        });
+      });
+
+      describe('if NOT empty', () => {
+        describe('if the value is more or equal than 1', () => {
+          beforeEach(() => {
+            component.selectionForm.get('quantity').setValue(2);
+          });
+
+          it('should be invalid', () => {
+            expect(component.selectionForm.get('quantity').valid).toEqual(true);
+          });
+        });
+
+        describe('if the value is less than 1', () => {
+          beforeEach(() => {
+            component.selectionForm.get('quantity').setValue(0);
+          });
+
+          it('should be invalid', () => {
+            expect(component.selectionForm.get('quantity').invalid).toEqual(true);
+          });
+        });
+      });
+    });
+  });
+
+  describe('when adding a product', () => {
+    let event: any;
+    let subscription: Subscription;
+    let product: IProduct;
+    let quantity: number;
+
+    beforeEach(() => {
+      event = jasmine.createSpy('event');
+      subscription = component.productAdd.subscribe(event);
+      product = TestUtils.createSpyObjWithoutMethods<IProduct>('product');
+      quantity = 10;
+      component.addProduct(product, quantity);
+    });
+
+    afterEach(() => {
+      subscription.unsubscribe();
+    });
+
+    it('should emit a productAdd event', () => {
+      expect(event).toHaveBeenCalledWith({ product, quantity });
+    });
   });
 });
